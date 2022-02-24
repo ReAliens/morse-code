fe5569845bdd7ff02a917ed7c3e1788702b6b756
Ahmed <ahmedreda152@gmail.com>
2022-02-24 14:33:16 +0200

    fix rubocop

--- decode.rb
+++ decode.rb
@@ -1,29 +1,25 @@
-$chars = {
+def decode_char(char)
+  chars = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
     '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
     '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
     '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
     '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
     '--..' => 'Z'
-}
-
-
-def decode_char(char)
-    $chars.each do |key,value|
-        return value if key == char
-    end 
+  }
+  chars.each do |key, value|
+    return value if key == char
+  end
 end
 
 def decode_word(coded_word)
-   actual_word =''
-   coded_word.split.each do |value|
-     char = decode_char(value)
-     actual_word+=char
-   end
-   return actual_word
+  actual_word = ''
+  coded_word.split.each do |value|
+    char = decode_char(value)
+    actual_word += char
+  end
+  actual_word
 end
 
-
 puts decode_char('.-')
-puts decode_word("-- -.--")
-
+puts decode_word('-- -.--')
